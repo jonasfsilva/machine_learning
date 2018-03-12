57490e9862cb5cd30357990bfbba44bd00218390
jonas <ferreirajonasss@gmail.com>
2018-03-12 20:05:57 -0300

    algoritmo de classificacao nayve bayes e exemplo de classificação

--- dados.py
+++ dados.py
@@ -2,3 +2,20 @@
 # acessou_contato, comprou
 
 # X são os dados que voce tem e y os dados que voce deseja prever
+
+import csv
+
+def carregar_accessos():
+    X = []
+    Y = []
+
+    arquivo = open('acesso_pagina.csv', 'r')
+    leitor = csv.reader(arquivo)
+    next(leitor)
+
+    for home, como_funciona, contato, comprou in leitor:
+        X.append([int(home), int(como_funciona),
+            int(contato)])
+        
+        Y.append(int(comprou))
+    return X, Y
