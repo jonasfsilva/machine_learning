d39e9e1871e5522908fcc46ed957cdeaeef34912
jonas <ferreirajonasss@gmail.com>
2018-03-25 21:40:42 -0300

    treinando o modelo com e usando pandas

--- dados.py
+++ dados.py
@@ -19,3 +19,19 @@ def carregar_accessos():
         
         Y.append(int(comprou))
     return X, Y
+
+def carregar_buscas():
+    X = []
+    Y = []
+
+    arquivo = open('buscas.csv', 'r')
+    leitor = csv.reader(arquivo)
+    next(leitor)
+
+    for home, busca, logado, comprou in leitor:
+        X.append([int(home), busca,
+            int(logado)])
+        
+        Y.append(int(comprou))
+    return X, Y
+
